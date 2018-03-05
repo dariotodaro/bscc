2bce2f13c7c36eb6d786b903e1ca3dfdf4eeb45b
Dario Todaro <dto@bluesky-it.ch>
2018-03-05 23:19:34 +0100

    Advent of Code Excercise1

---
+++ adventOfCode/src/hello/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import  (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	input := os.Args[1]
+	var inputLength = len(input)
+	var intArray = make([]int, inputLength)
+	var current int
+	var sum int
+	for i := 0; i<inputLength; i++ {
+		c, err := strconv.Atoi(string(input[i%inputLength]))
+		if err != nil {
+			fmt.Printf("Error in string convertion")
+		}else{
+			intArray[i] = c
+		}
+	}
+	for i := 0; i<inputLength; i++ {
+		current = intArray[i]
+		var next = intArray[(i+1)%inputLength]
+		if (current == next) {
+			sum += next
+		}
+	}
+	fmt.Printf("Sum is: %v", sum)
+}
